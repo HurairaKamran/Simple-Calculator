c773112dfa3f12610ef17479042960a1716fa241
Huraira Kamran <hurairakamran2006@gmail.com>
2024-06-03 01:40:56 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { name: "firstnumber", type: "number", message: "Enter your first number:" },
+    {
+        name: "secondnumber",
+        type: "number",
+        message: "Enter your second number:",
+    },
+    {
+        name: "operators",
+        type: "list",
+        message: "select the operator:",
+        choices: ["Addition", "Subtraction", "Multiplacation", "Division"],
+    },
+]);
+if (answer.operators === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operators === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operators === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operators === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("Enter correct operator");
+}
